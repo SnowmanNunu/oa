197c911221ef891b6018679b4a3652401266e5a4
SnowmanNunu <345750542@qq.com>
2018-09-12 18:18:50 +0800

    login

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -13,7 +13,28 @@ class Login extends Controller
  	
  	public function checkLogin()
  	{
+ 		if (request()->isPost()) {
+ 		$data = input('post.');
+ 		dump($data);
+ 		$this->check(input('code'));
+ 		if ($data['password']==md5($data['password'])) {
+		    session('name',$data['name']);
+		    $this->success('登录成功！',url('Login/index'));
+ 		}else{
+ 			$this->success('登录失败！',url('Login/index'));
+ 		}
+
+ 		}
  		
  	}
-}
 
+ 	// 验证码检测
+        public function check($code){
+
+        if (!captcha_check($code)) {
+        $this->error('验证码错误');
+        } else {
+        return true;
+        }
+    }
+}
